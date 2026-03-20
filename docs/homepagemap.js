a1cc31e216dbca1bc4abc274d4f05825b771f3c2
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-03-20 14:28:25 +0000

    Added emergency button functionality

--- docs/homepagemap.js
+++ docs/homepagemap.js
@@ -1,5 +1,13 @@
 let map = L.map("map").setView([53.33977105427492, -6.2949987583892755], 5);
 let markers = []
+let hospitalTimes = []
+let userLocation
+const currentTime = new Date()
+const timestamp = (currentTime.getHours() * 100) + (currentTime.getMinutes())
+
+console.log(timestamp)
+
+const button = document.getElementById("emergency");
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
@@ -17,5 +25,44 @@ fetch("./hospitals.json").then((response) => {
         console.log(i)
         markers.push(L.marker([data.hospitals[i].lat, data.hospitals[i].lon]).addTo(map))
         markers[i].bindPopup("<b><a href='./hospital_page.html?hospital_id=" + i + "'>" + data.hospitals[i].name + "</a></b><br>" + data.hospitals[i].hours.opening + " - " + data.hospitals[i].hours.closing + "<br>" + data.hospitals[i].website).openPopup();
+        hospitalTimes.push([data.hospitals[i].hours.opening, data.hospitals[i].hours.closing])
     }
 })
+
+button.addEventListener("click", findNearestHospital);
+
+function findNearestHospital(){
+    if(userLocation){
+        findClosest()
+    }
+    else if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        return
+    }
+}
+
+function success(position) {
+  console.log(position.coords.latitude, position.coords.longitude);
+  userLocation = [position.coords.latitude, position.coords.longitude]
+  findClosest()
+}
+
+function findClosest(){
+    let indexOfClosest, distanceOfClosest
+    distanceOfClosest = Infinity
+    for(let i = 0; i < markers.length; i++){
+        console.log(markers[i]._latlng)
+        let distance = Math.abs(userLocation[0] - markers[i]._latlng.lat) + Math.abs(userLocation[1] - markers[i]._latlng.lng)
+        if(distance < distanceOfClosest && timestamp > hospitalTimes[i][0] && timestamp < hospitalTimes[i][1] ){
+            indexOfClosest = i;
+            distanceOfClosest = distance;
+        }
+    }
+    map.panTo(markers[indexOfClosest]._latlng)
+    markers[indexOfClosest].openPopup()
+}
+
+function error() {
+  alert("Sorry, no position available.");
+}

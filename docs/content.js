54e40f989d6c387088cab4a8f8d3efa8a1cc51c3
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-03-20 14:54:24 +0000

    Commenting content.js

--- docs/content.js
+++ docs/content.js
@@ -1,18 +1,22 @@
+//DOM Elements
 const euContent = document.getElementById("eucontent");
 const nonEuContent = document.getElementById("noneucontent");
 
 const euButton = document.getElementById("eu");
 const nonEuButton = document.getElementById("noneu");
 
+//Event Listeners
 euButton.addEventListener("click", euSelected);
 nonEuButton.addEventListener("click", nonEuSelected);
 
 console.log("WORKING")
 
 function euSelected(){
+    //Show the EU Content, hide the non EU Content
     euContent.style.display = "";
     nonEuContent.style.display = "none";
 
+    //Change styling for the buttons
     euButton.classList.add("active");
     nonEuButton.classList.add("inactive");
     euButton.classList.remove("inactive");
@@ -21,9 +25,11 @@ function euSelected(){
 }
 
 function nonEuSelected(){
+    //Show the Non EU Content, hide the EU Content
     euContent.style.display = "none";
     nonEuContent.style.display = "";
 
+    //Change styling for the buttons
     nonEuButton.classList.add("active");
     euButton.classList.add("inactive");
     nonEuButton.classList.remove("inactive");

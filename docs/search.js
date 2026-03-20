f5eddce904fc1d6e8e51543a354c88a499d85509
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-03-20 14:34:13 +0000

    Commented search.js

--- docs/search.js
+++ docs/search.js
@@ -1,3 +1,4 @@
+//Fetching DOM elements
 const searchbar = document.getElementById("search-bar")
 const searchoutput = document.getElementById("search-output")
 const searchtemplate = document.getElementById("search-result-template")
@@ -8,18 +9,26 @@ searchbar.addEventListener("input", searchHospital)
 
 function searchHospital(){
     console.log(searchbar.value)
+    //Clear any existing results in the search bar
     searchoutput.innerHTML = ""
+    //Get the hospital JSON information
     fetch("./hospitals.json").then((response) => {
         return response.json()
     }).then(data => {
         if(searchbar.value != ""){
+            //Loop through the hospitals
             for(let i = 0; i < data.hospitals.length; i++){
                 console.log(i)
+                //If the hospital name starts with the string included in the searchbar...
                 if(data.hospitals[i].name.toLowerCase().startsWith(searchbar.value.toLowerCase())){
+                    //Make a copy of the template included in the html page
                     let templateCopy = searchtemplate.content.cloneNode(true).children[0]
                     console.log(templateCopy)
+                    //Set the text to the hospitals name
                     templateCopy.innerHTML = data.hospitals[i].name
+                    //Set the href to a link to the hospital page with the value of the hospital's ID in the included link
                     templateCopy.href = "./hospital_page.html?hospital_id=" + i
+                    //Add it to the search bar element
                     searchoutput.appendChild(templateCopy)
                 }
             }
